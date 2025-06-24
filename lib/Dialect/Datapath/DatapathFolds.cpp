f69e1372adcc9560777b902754dbe0c23462ddec
cowardsa <s.coward111@gmail.com>
2025-06-24 10:58:22 +0100

    Only fold compressors wiht a single use

--- lib/Dialect/Datapath/DatapathFolds.cpp
+++ lib/Dialect/Datapath/DatapathFolds.cpp
@@ -34,28 +34,34 @@ struct FoldCompressIntoCompress : public OpRewritePattern<datapath::CompressOp> 
     auto operands = compOp.getOperands();
     SmallVector<Value, 8> processedCompressorResults;
     SmallVector<Value, 8> newCompressOperands;
-    auto numCompress = 0;
-    bool hasRegArgs = false;
 
     // Check if any operand is a CompressOp and collect all operands
     for (Value operand : operands) {
       if (llvm::is_contained(processedCompressorResults, operand))
         continue; // Skip if already processed this compressor
+      
+      if (!operand.hasOneUse()) {
+        // If the operand has multiple uses, we cannot fold it into a compress
+        // operation, so we treat it as a regular operand.
+        newCompressOperands.push_back(operand);
+        continue;
+      }
 
       if (auto compressOp = operand.getDefiningOp<datapath::CompressOp>()) {
         // Found a compress op - add its operands to our new list
-        ++numCompress;
         llvm::append_range(newCompressOperands, compressOp.getOperands());
         // Only process each compressor once
         llvm::append_range(processedCompressorResults, compressOp.getResults());
-      } else if (auto addOp = operand.getDefiningOp<comb::AddOp>()) {
+        continue;
+      } 
+     
+      if (auto addOp = operand.getDefiningOp<comb::AddOp>()) {
         llvm::append_range(newCompressOperands, addOp.getOperands());
+        continue;
       }
-      else {
-        hasRegArgs = true;
-        // Regular operand - just add it to our list
-        newCompressOperands.push_back(operand);
-      }
+
+      // Regular operand - just add it to our list
+      newCompressOperands.push_back(operand);
     }
 
     // If no compress was found, this pattern doesn't apply
